b030efb1cc3d1cb3af5d0e1f3bc05eb4c97725c3
cristian <cristian@HP-ProBook.(none)>
2013-12-20 19:49:12 +0200

    Adding omp region to JDiff.cpp

--- src/JDiff.cpp
+++ src/JDiff.cpp
@@ -538,7 +538,7 @@ int JDiff::ufFndAhdScn ()
 
   /* Build hashtable */
   liIdx = 0;
-//#pragma omp parallel default(shared) private(lcValOrg, lkHshOrg, lzPosOrg, liEqlOrg, liIdx)
+#pragma omp parallel default(shared) private(lcValOrg, lkHshOrg, lzPosOrg, liEqlOrg, liIdx)
 {
   while (lcValOrg > EOF) {
     gpHsh->hash(lcValOrg, lkHshOrg) ;
@@ -563,11 +563,11 @@ int JDiff::ufFndAhdScn ()
             }
         }
     }
-//#pragma omp flush(lcValOrg)
+#pragma omp flush(lcValOrg)
   }
 }
 
-//#pragma omp barrier
+#pragma omp barrier
 
   if (miVerbse > 0) fprintf(JDebug::stddbg, ".\n");
 
